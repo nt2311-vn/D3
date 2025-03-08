990c325fcef23ad6ba16e6c799aa4cfa7d9ed088
nt2311 <nguyentoan231196@gmail.com>
2025-03-08 10:41:04 +0700

    complete the scatter plot

--- scatter_plot/src/main.js
+++ scatter_plot/src/main.js
@@ -1,48 +1,46 @@
 import * as d3 from "d3";
 import data from "../data.json";
 
-// Height, width and padding
-const w = 1920;
-const h = 1080;
-const padding = { top: 30, bottom: 60, right: 30, left: 60 };
+// Height, width, and padding
+const w = 1200;
+const h = 600;
+const padding = { top: 50, bottom: 100, right: 50, left: 100 };
 
 // Parse time function
 const parseTime = d3.timeParse("%Y");
 const yearParsed = data.map((d) => parseTime(`${d.Year}`));
 
-const parseMinute = d3.timeParse("%M:%S");
-const timeParsed = data.map((d) => parseMinute(d.Time));
-
-// Data
-const years = data.map((d) => d.Year);
-const times = data.map((d) => +d.Time);
+// Convert time to Date objects for y-axis
+const timeParsed = data.map((d) => {
+	const [minutes, seconds] = d.Time.split(":").map(Number);
+	return new Date(1970, 0, 1, 0, minutes, seconds); // Use a fixed date for time-only values
+});
 
 // Create svg
-const svg = d3
-	.select("body")
-	.append("svg")
-	.attr("width", w)
-	.attr("height", h)
-	.attr("id", "title");
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-// Create scale
+// Add title (User Story #1)
+svg
+	.append("text")
+	.attr("id", "title")
+	.attr("x", w / 2)
+	.attr("y", padding.top)
+	.attr("text-anchor", "middle")
+	.style("font-size", "24px")
+	.text("Doping in Professional Bicycle Racing");
+
+// Create scales
 const xScale = d3
-	.scaleBand()
+	.scaleTime()
 	.domain([d3.min(yearParsed), d3.max(yearParsed)])
 	.range([padding.left, w - padding.right]);
 
-const xTimeScale = d3
-	.scaleTime()
-	.domain(d3.min(years), d3.max(years))
-	.range([padding.left, w - padding.right]);
-
 const yScale = d3
-	.scaleLinear()
-	.domain(0, d3.max(timeParsed))
+	.scaleTime()
+	.domain([d3.min(timeParsed), d3.max(timeParsed)])
 	.range([h - padding.bottom, padding.top]);
 
-// Add dots
-
+// Add dots (User Story #4, #5, #6, #7, #8)
 svg
 	.selectAll("dot")
 	.data(data)
@@ -50,17 +48,80 @@ svg
 	.append("circle")
 	.attr("class", "dot")
 	.attr("r", 6)
-	.attr("cx", (d) => xScale(parseTime(d.Year)))
-	.attr("cy", (d) => yScale(d.Time))
-	.attr("fill", "steelblue");
+	.attr("cx", (d) => xScale(parseTime(`${d.Year}`)))
+	.attr("cy", (d) => {
+		const [minutes, seconds] = d.Time.split(":").map(Number);
+		return yScale(new Date(1970, 0, 1, 0, minutes, seconds));
+	})
+	.attr("data-xvalue", (d) => parseTime(`${d.Year}`)) // Use Date object for x-value
+	.attr("data-yvalue", (d) => {
+		const [minutes, seconds] = d.Time.split(":").map(Number);
+		return new Date(1970, 0, 1, 0, minutes, seconds); // Use Date object for y-value
+	})
+	.attr("fill", (d) => (d.Doping ? "red" : "steelblue")) // Color based on doping allegations
+	.on("mouseover", (event, d) => {
+		tooltip
+			.style("opacity", 1)
+			.attr("data-year", parseTime(`${d.Year}`)) // User Story #15
+			.html(
+				`${d.Name}: ${d.Nationality}<br>Year: ${d.Year}, Time: ${d.Time}${
+					d.Doping ? `<br><br>${d.Doping}` : ""
+				}`,
+			)
+			.style("left", `${event.pageX + 10}px`)
+			.style("top", `${event.pageY - 20}px`);
+	})
+	.on("mouseout", () => {
+		tooltip.style("opacity", 0);
+	});
 
-// Add axis
+// Add x-axis (User Story #2, #10, #11)
 const xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(d3.timeFormat("%Y"));
-const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
-
 svg
 	.append("g")
+	.attr("id", "x-axis")
 	.attr("transform", `translate(0, ${h - padding.bottom})`)
 	.call(xAxis);
 
-svg.append("g").attr("transform", `translate(${padding.left}, 0)`).call(yAxis);
+// Add y-axis (User Story #3, #9, #12)
+const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
+svg
+	.append("g")
+	.attr("id", "y-axis")
+	.attr("transform", `translate(${padding.left}, 0)`)
+	.call(yAxis);
+
+// Add legend (User Story #13)
+const legend = svg
+	.append("g")
+	.attr("id", "legend")
+	.attr("transform", `translate(${w - padding.right - 100}, ${padding.top})`);
+
+legend
+	.append("rect")
+	.attr("width", 20)
+	.attr("height", 20)
+	.attr("fill", "steelblue");
+
+legend.append("text").attr("x", 30).attr("y", 15).text("No Doping Allegations");
+
+legend
+	.append("rect")
+	.attr("y", 30)
+	.attr("width", 20)
+	.attr("height", 20)
+	.attr("fill", "red");
+
+legend.append("text").attr("x", 30).attr("y", 45).text("Doping Allegations");
+
+// Add tooltip (User Story #14, #15)
+const tooltip = d3
+	.select("body")
+	.append("div")
+	.attr("id", "tooltip")
+	.style("opacity", 0)
+	.style("position", "absolute")
+	.style("background", "#fff")
+	.style("border", "1px solid #ccc")
+	.style("padding", "10px")
+	.style("border-radius", "5px");

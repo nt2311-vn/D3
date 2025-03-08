3a8e70112d596a5593090a80cb75897cd0b3b210
nt2311 <nguyentoan231196@gmail.com>
2025-03-08 10:09:24 +0700

    extract data and add information of padding, width and height

--- scatter_plot/src/main.js
+++ scatter_plot/src/main.js
@@ -1,5 +1,20 @@
 import * as d3 from "d3";
 import data from "../data.json";
 
+// Height, width and padding
 const w = 1920;
 const h = 1080;
+const padding = { top: 30, bottom: 60, right: 30, left: 60 };
+
+// Data
+const years = data.map((d) => d.Year);
+const times = data.map((d) => +d.Time);
+
+const svg = d3
+	.select("body")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h)
+	.attr("id", "title");
+
+svg.selectAll();

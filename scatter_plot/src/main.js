856d22e3d7468a9c856f6ac83b5f8c66215b8eda
nt2311 <nguyentoan231196@gmail.com>
2025-03-08 10:21:34 +0700

    time scale axis and value doping axis, add dots to svg

--- scatter_plot/src/main.js
+++ scatter_plot/src/main.js
@@ -10,6 +10,7 @@ const padding = { top: 30, bottom: 60, right: 30, left: 60 };
 const years = data.map((d) => d.Year);
 const times = data.map((d) => +d.Time);
 
+// Create svg
 const svg = d3
 	.select("body")
 	.append("svg")
@@ -17,4 +18,29 @@ const svg = d3
 	.attr("height", h)
 	.attr("id", "title");
 
-svg.selectAll();
+// Create scale
+const xScale = d3
+	.scaleBand()
+	.domain(d3.range(data.length))
+	.range([padding.left, w - padding.right])
+	.padding(0.1);
+
+const xTimeScale = d3
+	.scaleTime()
+	.domain(d3.min(years), d3.max(years))
+	.range([padding.left, w - padding.right]);
+
+const yScale = d3
+	.scaleLinear()
+	.domain(0, d3.max(times))
+	.range([h - padding.bottom, padding.top]);
+
+// Add dots
+
+svg
+	.selectAll("dot")
+	.data(data)
+	.enter()
+	.append("circle")
+	.attr("class", "dot")
+	.attr("r", 6);
